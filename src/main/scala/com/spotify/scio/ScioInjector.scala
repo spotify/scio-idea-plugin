90d2332545bdf7900b135b738938e23d6bab72f7
Filipe Regadas <filiperegadas@gmail.com>
2019-03-28 18:39:10 -0400

    Small code reorg (#31)

--- src/main/scala/com/spotify/scio/ScioInjector.scala
+++ src/main/scala/com/spotify/scio/ScioInjector.scala
@@ -34,28 +34,26 @@ import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef.SyntheticMembe
 
 import scala.collection.mutable
 
-class ScioInjector extends SyntheticMembersInjector {
-  private val logger = Logger.getInstance(classOf[ScioInjector])
+object ScioInjector {
+  private val Log = Logger.getInstance(classOf[ScioInjector])
 
   // Could not find a way to get fully qualified annotation names
   // even tho there is API, it does not return the annotations.
   // For now stick with relative annotation names.
   private val BQTNamespace = "BigQueryType"
-  private val fromQuery = s"$BQTNamespace.fromQuery"
-  private val fromTable = s"$BQTNamespace.fromTable"
-  private val annotations = Seq(fromQuery,
-                                fromTable,
+  private val FromQuery = s"$BQTNamespace.fromQuery"
+  private val FromTable = s"$BQTNamespace.fromTable"
+  private val Annotations = Seq(FromQuery,
+                                FromTable,
                                 s"$BQTNamespace.fromSchema",
                                 s"$BQTNamespace.toTable")
 
   private val AvroTNamespace = "AvroType"
-  private val avroAnnotations = Seq(s"$AvroTNamespace.fromSchema",
+  private val AvroAnnotations = Seq(s"$AvroTNamespace.fromSchema",
                                     s"$AvroTNamespace.fromPath",
                                     s"$AvroTNamespace.toSchema")
 
-  private val alertEveryMissedXInvocations = 5
-  private val classMissed =
-    mutable.HashMap.empty[String, Int].withDefaultValue(0)
+  private val AlertEveryMissedXInvocations = 5
 
   /**
     * Finds BigQuery cache file, must be in sync with Scio implementation, otherwise plugin will
@@ -88,32 +86,10 @@ class ScioInjector extends SyntheticMembersInjector {
     }
   }
 
-  private def findClassFile(fileName: String): Option[java.io.File] = {
-    val classFile = getClassCacheFile(fileName).toFile
-    val classFilePath = classFile.getAbsolutePath
-    if (classFile.exists()) {
-      logger.debug(s"Found $classFilePath")
-      classMissed(fileName) = 0
-      Some(classFile)
-    } else {
-      classMissed(fileName) += 1
-      val errorMessage =
-        s"""|Scio plugin could not find scala files for code completion. Please (re)compile the project.
-                             |Missing: $classFilePath""".stripMargin
-      if (classMissed(fileName) >= alertEveryMissedXInvocations) {
-        // reset counter
-        classMissed(fileName) = 0
-        logger.error(errorMessage)
-      }
-      logger.warn(errorMessage)
-      None
-    }
-  }
-
   /**
     * Computes hash for macro - the hash must be consistent with hash implementation in Scio.
     */
-  private def genHashForMacro(owner: String, srcFile: String): String = {
+  private def genHashForMacro(owner: String, srcFile: String): String =
     Hashing
       .murmur3_32()
       .newHasher()
@@ -121,119 +97,27 @@ class ScioInjector extends SyntheticMembersInjector {
       .putString(srcFile, Charsets.UTF_8)
       .hash()
       .toString
-  }
 
-  /**
-    * Main method of the plugin. Injects syntactic inner members like case classes and companion
-    * objects, makes IntelliJ happy about BigQuery macros. Assumes macro is enclosed within
-    * class/object.
-    */
-  override def injectInners(source: ScTypeDefinition): Seq[String] = {
-    source.extendsBlock.members.flatMap {
-      case c: ScClass
-          if c.annotations
-            .map(_.getText)
-            .exists(t => annotations.exists(t.contains)) =>
-        val caseClasses = fetchGeneratedCaseClasses(source, c)
-        val extraCompanionMethod = fetchExtraBQTypeCompanionMethods(source, c)
-        val tupledMethod = getTupledMethod(c.getName, caseClasses)
-
-        val applyPropsSignature = getApplyPropsSignature(caseClasses)
-        val unapplyReturnTypes =
-          getUnapplyReturnTypes(caseClasses).mkString(" , ")
-
-        // TODO: missing extends and traits - are they needed?
-        // $tn extends ${p(c, SType)}.HasSchema[$name] with ..$traits
-        val companion = s"""|object ${c.getName} {
-                            |  def apply( $applyPropsSignature ): ${c.getName} = ???
-                            |  def unapply(x$$0: ${c.getName}): _root_.scala.Option[($unapplyReturnTypes)] = ???
-                            |  def fromTableRow: _root_.scala.Function1[_root_.com.google.api.services.bigquery.model.TableRow, ${c.getName} ] = ???
-                            |  def toTableRow: _root_.scala.Function1[ ${c.getName}, _root_.com.google.api.services.bigquery.model.TableRow] = ???
-                            |  def schema: _root_.com.google.api.services.bigquery.model.TableSchema = ???
-                            |  def toPrettyString(indent: Int = 0): String = ???
-                            |  $extraCompanionMethod
-                            |  $tupledMethod
-                            |}""".stripMargin
-
-        if (caseClasses.isEmpty) {
-          Seq.empty
-        } else {
-          caseClasses ++ Seq(companion)
-        }
-
-      case c: ScClass
-          if c.annotations
-            .map(_.getText)
-            .exists(t => avroAnnotations.exists(t.contains)) =>
-        val caseClasses = fetchGeneratedCaseClasses(source, c)
-        val tupledMethod = getTupledMethod(c.getName, caseClasses)
-        val applyPropsSignature = getApplyPropsSignature(caseClasses)
-        val unapplyReturnTypes =
-          getUnapplyReturnTypes(caseClasses).mkString(" , ")
-
-        val companion = s"""|object ${c.getName} {
-                            |  def apply( $applyPropsSignature ): ${c.getName} = ???
-                            |  def unapply(x$$0: ${c.getName}): _root_.scala.Option[($unapplyReturnTypes)] = ???
-                            |  def fromGenericRecord: _root_.scala.Function1[_root_.org.apache.avro.generic.GenericRecord, ${c.getName} ] = ???
-                            |  def toGenericRecord: _root_.scala.Function1[ ${c.getName}, _root_.org.apache.avro.generic.GenericRecord] = ???
-                            |  def schema: _root_.org.apache.avro.Schema = ???
-                            |  def toPrettyString(indent: Int = 0): String = ???
-                            |  $tupledMethod
-                            |}""".stripMargin
-
-        if (caseClasses.isEmpty) {
-          Seq.empty
-        } else {
-          caseClasses ++ Seq(companion)
-        }
-      case _ => Seq.empty
-    }
-  }
-
-  private def getApplyPropsSignature(caseClasses: Seq[String]) = {
+  private def getApplyPropsSignature(caseClasses: Seq[String]) =
     getConstructorProps(caseClasses)
       .map(_.props)
       .getOrElse(Seq.empty)
       .mkString(" , ")
-  }
 
   private def fetchExtraBQTypeCompanionMethods(source: ScTypeDefinition,
                                                c: ScClass) = {
     val annotation =
-      c.annotations.map(_.getText).find(t => annotations.exists(t.contains)).get
-    logger.debug(s"Found $annotation in ${source.getQualifiedNameForDebugger}")
+      c.annotations.map(_.getText).find(t => Annotations.exists(t.contains)).get
+    Log.debug(s"Found $annotation in ${source.getQualifiedNameForDebugger}")
 
     val extraCompanionMethod = annotation match {
-      case a if a.contains(fromQuery) =>
+      case a if a.contains(FromQuery) =>
         "def query: _root_.java.lang.String = ???"
-      case a if a.contains(fromTable) =>
+      case a if a.contains(FromTable) =>
         "def table: _root_.java.lang.String = ???"
       case _ => ""
     }
     extraCompanionMethod
-  }
-
-  private def fetchGeneratedCaseClasses(source: ScTypeDefinition,
-                                        c: ScClass) = {
-    // For some reason sometimes [[getVirtualFile]] returns null, use Option. I don't know why.
-    val fileName =
-      Option(c.asInstanceOf[PsiElement].getContainingFile.getVirtualFile)
-      // wrap VirtualFile to java.io.File to use OS file separator
-        .map(vf => new File(vf.getCanonicalPath).getCanonicalPath)
-
-    val hash =
-      fileName.map(genHashForMacro(source.getQualifiedNameForDebugger, _))
-
-    hash
-      .flatMap(h => findClassFile(s"${c.getName}-$h.scala"))
-      .map(f => {
-        import collection.JavaConverters._
-        Files
-          .readLines(f, Charset.defaultCharset())
-          .asScala
-          .filter(_.contains("case class"))
-      })
-      .getOrElse(Seq.empty)
   }
 
   private def getConstructorProps(
@@ -279,10 +163,131 @@ class ScioInjector extends SyntheticMembersInjector {
       case _ =>
         ""
     }
+
     maybeTupledMethod.getOrElse("")
   }
 
-  case class ConstructorProps(props: Seq[String]) {
+  final case class ConstructorProps(props: Seq[String]) {
     val types: Seq[String] = props.map(_.split(" : ")(1).trim)
   }
+}
+
+final class ScioInjector extends SyntheticMembersInjector {
+  import ScioInjector._
+
+  private[this] val classMissed =
+    mutable.HashMap.empty[String, Int].withDefaultValue(0)
+
+  /**
+    * Main method of the plugin. Injects syntactic inner members like case classes and companion
+    * objects, makes IntelliJ happy about BigQuery macros. Assumes macro is enclosed within
+    * class/object.
+    */
+  override def injectInners(source: ScTypeDefinition): Seq[String] = {
+    source.extendsBlock.members.flatMap {
+      case c: ScClass
+          if c.annotations
+            .map(_.getText)
+            .exists(t => Annotations.exists(t.contains)) =>
+        val caseClasses = fetchGeneratedCaseClasses(source, c)
+        val extraCompanionMethod = fetchExtraBQTypeCompanionMethods(source, c)
+        val tupledMethod = getTupledMethod(c.getName, caseClasses)
+
+        val applyPropsSignature = getApplyPropsSignature(caseClasses)
+        val unapplyReturnTypes =
+          getUnapplyReturnTypes(caseClasses).mkString(" , ")
+
+        // TODO: missing extends and traits - are they needed?
+        // $tn extends ${p(c, SType)}.HasSchema[$name] with ..$traits
+        val companion = s"""|object ${c.getName} {
+                            |  def apply( $applyPropsSignature ): ${c.getName} = ???
+                            |  def unapply(x$$0: ${c.getName}): _root_.scala.Option[($unapplyReturnTypes)] = ???
+                            |  def fromTableRow: _root_.scala.Function1[_root_.com.google.api.services.bigquery.model.TableRow, ${c.getName} ] = ???
+                            |  def toTableRow: _root_.scala.Function1[ ${c.getName}, _root_.com.google.api.services.bigquery.model.TableRow] = ???
+                            |  def schema: _root_.com.google.api.services.bigquery.model.TableSchema = ???
+                            |  def toPrettyString(indent: Int = 0): String = ???
+                            |  $extraCompanionMethod
+                            |  $tupledMethod
+                            |}""".stripMargin
+
+        if (caseClasses.isEmpty) {
+          Seq.empty
+        } else {
+          caseClasses ++ Seq(companion)
+        }
+
+      case c: ScClass
+          if c.annotations
+            .map(_.getText)
+            .exists(t => AvroAnnotations.exists(t.contains)) =>
+        val caseClasses = fetchGeneratedCaseClasses(source, c)
+        val tupledMethod = getTupledMethod(c.getName, caseClasses)
+        val applyPropsSignature = getApplyPropsSignature(caseClasses)
+        val unapplyReturnTypes =
+          getUnapplyReturnTypes(caseClasses).mkString(" , ")
+
+        val companion = s"""|object ${c.getName} {
+                            |  def apply( $applyPropsSignature ): ${c.getName} = ???
+                            |  def unapply(x$$0: ${c.getName}): _root_.scala.Option[($unapplyReturnTypes)] = ???
+                            |  def fromGenericRecord: _root_.scala.Function1[_root_.org.apache.avro.generic.GenericRecord, ${c.getName} ] = ???
+                            |  def toGenericRecord: _root_.scala.Function1[ ${c.getName}, _root_.org.apache.avro.generic.GenericRecord] = ???
+                            |  def schema: _root_.org.apache.avro.Schema = ???
+                            |  def toPrettyString(indent: Int = 0): String = ???
+                            |  $tupledMethod
+                            |}""".stripMargin
+
+        if (caseClasses.isEmpty) {
+          Seq.empty
+        } else {
+          caseClasses ++ Seq(companion)
+        }
+      case _ => Seq.empty
+    }
+  }
+
+  private def fetchGeneratedCaseClasses(source: ScTypeDefinition,
+                                        c: ScClass) = {
+    // For some reason sometimes [[getVirtualFile]] returns null, use Option. I don't know why.
+    val fileName =
+      Option(c.asInstanceOf[PsiElement].getContainingFile.getVirtualFile)
+      // wrap VirtualFile to java.io.File to use OS file separator
+        .map(vf => new File(vf.getCanonicalPath).getCanonicalPath)
+
+    val hash =
+      fileName.map(genHashForMacro(source.getQualifiedNameForDebugger, _))
+
+    hash
+      .flatMap(h => findClassFile(s"${c.getName}-$h.scala"))
+      .map(f => {
+        import collection.JavaConverters._
+        Files
+          .readLines(f, Charset.defaultCharset())
+          .asScala
+          .filter(_.contains("case class"))
+      })
+      .getOrElse(Seq.empty)
+  }
+
+  private def findClassFile(fileName: String): Option[java.io.File] = {
+    val classFile = getClassCacheFile(fileName).toFile
+    val classFilePath = classFile.getAbsolutePath
+    if (classFile.exists()) {
+      Log.debug(s"Found $classFilePath")
+      classMissed(fileName) = 0
+      Some(classFile)
+    } else {
+      classMissed(fileName) += 1
+      val errorMessage =
+        s"""|Scio plugin could not find scala files for code completion. Please (re)compile the project.
+            |Missing: $classFilePath""".stripMargin
+      if (classMissed(fileName) >= AlertEveryMissedXInvocations) {
+        // reset counter
+        classMissed(fileName) = 0
+        Log.error(errorMessage)
+      }
+      Log.warn(errorMessage)
+      None
+    }
+  }
+
 }
